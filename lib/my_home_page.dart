008b157d851f03dddbe61b687a3ac076a90d035b
markryo <dev.mryo@gmail.com>
2022-03-01 22:00:01 +0900

    Adding listile with custom widget

---
+++ lib/my_home_page.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_zero_to_hero_app_4/quiz_page.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("Creativity Quiz"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset("images/egg.png"),
+            ElevatedButton(
+              onPressed: () {
+                print('hi');
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return QuizPage();
+                    },
+                  ),
+                );
+              },
+              child: Text(
+                "Click Me",
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
