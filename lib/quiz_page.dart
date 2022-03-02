e5e761deaf6cbbc5e58f29939fdf6b8a1dc93022
markryo <dev.mryo@gmail.com>
2022-03-02 22:49:43 +0900

    finalized version for the practice

--- lib/quiz_page.dart
+++ lib/quiz_page.dart
@@ -1,28 +1,127 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
-class QuizPage extends StatelessWidget {
+class TileInfo {
+  String title;
+  Color color;
+
+  TileInfo({required this.title, required this.color});
+}
+
+List<TileInfo> tileInfoList = [
+  TileInfo(title: 'Red', color: Colors.red),
+  TileInfo(title: 'Green', color: Colors.green),
+  TileInfo(title: 'Blue', color: Colors.blue),
+  TileInfo(title: 'Orange', color: Colors.orange),
+];
+
+int score = 0;
+
+class QuizPage extends StatefulWidget {
   const QuizPage({Key? key}) : super(key: key);
 
   @override
+  State<QuizPage> createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  // if 0, show the first question page. if not , go to next question
+  int currentPage = 0;
+
+  List<bool?> boolToShow = List.generate(8, (index) => Random().nextBool());
+  List<bool?> boolCheckList = List.generate(8, (index) => false);
+
+  @override
   Widget build(BuildContext context) {
+    bool everythingIsFine = true;
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
         title: Text('Quiz Page'),
       ),
-      body: Container(
-        width: double.infinity,
-        child: (Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text("Select the red Square"),
-            _customListTile(title: 'Red', color: Colors.red),
-            _customListTile(title: 'Blue', color: Colors.blue),
-            _customListTile(title: 'Green', color: Colors.green),
-            _customListTile(title: 'Orange', color: Colors.orange)
-          ],
-        )),
-      ),
+      body: currentPage == 0
+          ? Container(
+              width: double.infinity,
+              child: (Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    "Select the red Square",
+                    style: TextStyle(
+                      fontSize: 25,
+                    ),
+                  ),
+                  Spacer(),
+                  ...List.generate(
+                    tileInfoList.length,
+                    (index) => _customListTile(
+                        title: tileInfoList[index].title,
+                        color: tileInfoList[index].color),
+                  ),
+                  Spacer(),
+                ],
+              )),
+            )
+          : currentPage == 1
+              ? Container(
+                  width: double.infinity,
+                  child: (Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Check Only the 'Yes' tile",
+                        style: TextStyle(
+                          fontSize: 25,
+                        ),
+                      ),
+                      Spacer(),
+                      ...List.generate(
+                        boolCheckList.length,
+                        (index) {
+                          for (int i = 0; i < boolCheckList.length; i++) {
+                            if (boolToShow[i] != boolCheckList[i]) {
+                              everythingIsFine = false;
+                            }
+                          }
+                          return CheckboxListTile(
+                            value: boolCheckList[index],
+                            onChanged: (valueBool) {
+                              setState(() {
+                                boolCheckList[index] = valueBool;
+                              });
+                            },
+                            title: Text(boolToShow[index]! ? "Yes" : "No"),
+                          );
+                        },
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          setState(() {
+                            if (everythingIsFine) {
+                              score += 1;
+                            }
+                            currentPage += 1;
+                            print(currentPage);
+                          });
+                        },
+                        child: Text("Click Me"),
+                      ),
+                      Spacer(),
+                    ],
+                  )),
+                )
+              : Container(
+                  child: Center(
+                    child: Text(
+                      'Score $score',
+                      style: TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.red),
+                    ),
+                  ),
+                ),
     );
   }
 
@@ -31,7 +130,18 @@ class QuizPage extends StatelessWidget {
         title: Text(title),
         tileColor: color,
         onTap: () {
-          print(title);
+          if (color == Colors.red) {
+            print('Yes, this is Red');
+            score = 1;
+            print(score);
+          } else {
+            print('OMG THIS IS NOT RED YOU TWAT');
+            score = 0;
+          }
+          setState(() {
+            // setState meands to reset the build
+            currentPage += 1;
+          });
         },
       );
 }
